2cfa429da96e0adc8953a7f79be4d98b02e850a8
Tayyab Malik <tayyabmalik3377@gmail.com>
2026-07-23 00:12:32 +0500

    Add files via upload

---
+++ sccript.js
@@ -0,0 +1,124 @@
+// PROBLEM-1:
+// for(let i=2; i<11; i+=2 ){
+//     console.log(i);
+    
+// }
+
+
+
+// PROBLEM-2:
+// let table = prompt("Enter the table number!")
+// for(let i =1;i<=10;i++){
+// console.log(`${table} * ${i} = ${table*i}`);
+// }
+
+
+
+// PROBLEM-3:
+// let a = +prompt("Enter 1st number!")
+// let b = +prompt("Enter 2nd number!")
+// let sum =0;
+// for(let i=a;i<=b;i++){
+//     console.log(i);
+//     sum +=i
+// }
+// alert(sum);
+
+
+
+//PROBLEM-4:
+// for(let i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+
+
+//  PROBLEM-5:
+// let user = +prompt("Enter the number!")
+// console.log(user*user)
+
+
+
+// PROBLEM-6:
+// let a = +prompt("Enter 1st number!")
+// let b = +prompt("Enter 2nd number!")
+// let evencount =0
+// let oddcount =0
+// for(let i =a;i<=b;i++){
+//     console.log(i);
+// if(i%2===0){
+//  evencount +=1
+// }else{
+//  oddcount +=1
+// }
+// }
+// alert(`Even Numbers =${evencount}`)
+// alert(`Odd Numbers =${oddcount}`)
+
+
+
+
+// PROBLEM-7:
+// let user = +prompt("Enter a number!")
+// let factorial = 1;
+// for(let i = user;i>=1;i--){
+// factorial*=i;
+// }
+// console.log(factorial);
+
+
+
+// PROBLEM-8:
+// for(let i=1;i<=6;i++){
+//     let stars ="";
+// for(let j =1;j<=i;j++){
+//     stars +="*";
+// }
+// console.log(stars);
+// }
+
+
+
+
+
+// PROBLEM-9:
+// let a = +prompt("Enter 1st number!");
+// let b = +prompt("Enter 2nd number!");
+
+// for (let i = a; i <= b; i++) {
+
+//     let isPrime = true;
+
+//     if (i <= 1) {
+//         isPrime = false;
+//     } else {
+
+//         for (let j = 2; j < i; j++) {
+
+//             if (i % j === 0) {
+//                 isPrime = false;
+//                 break;
+//             }
+
+//         }
+
+//     }
+
+//     if (isPrime) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+
+// PROBLEM-10:
+// let a = +prompt("Enter 1st number!")
+// let b = +prompt("Enter 2nd number!")
+// for(let i =a;i<=b;i++){
+//   if(i%2!==0){
+// console.log(i);  
+// }
+// }
